e224d211da06626fc0914bc04190f1b89da14b83
Zyskn <gabrielrojas1234.gg@gmail.com>
2026-07-05 16:40:43 -0300

    Delete scripts directory

--- scripts/diagnostics.sql
+++
@@ -1,7 +0,0 @@
--- Queries sueltas de chequeo / diagnóstico (no son migraciones, no cambian el schema)
-
--- Contar productos
-select count(*) from products;
-
--- Verificar fila de settings
-SELECT * FROM settings WHERE key = 'store_locked';
